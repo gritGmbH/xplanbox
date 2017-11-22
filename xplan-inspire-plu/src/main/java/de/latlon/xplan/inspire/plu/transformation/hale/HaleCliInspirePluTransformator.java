1df8759da67ac1a54b6aa277b79b3d16c7f8d850
Lyn Elisa Goltz <goltz@lat-lon.de>
2017-11-22 10:29:43 +0100

    #107 - implemented HaleCliInspirePluTransformator

---
+++ xplan-inspire-plu/src/main/java/de/latlon/xplan/inspire/plu/transformation/hale/HaleCliInspirePluTransformator.java
@@ -0,0 +1,57 @@
+package de.latlon.xplan.inspire.plu.transformation.hale;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.latlon.xplan.inspire.plu.transformation.InspirePluTransformator;
+import de.latlon.xplan.inspire.plu.transformation.TransformationException;
+
+/**
+ * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
+ */
+public class HaleCliInspirePluTransformator implements InspirePluTransformator {
+
+    private static final Logger LOG = LoggerFactory.getLogger( HaleCliInspirePluTransformator.class );
+
+    private final String haleCli;
+
+    private final String haleProject;
+
+    public HaleCliInspirePluTransformator( String haleCli ) {
+        this.haleCli = haleCli;
+        this.haleProject = HaleCliInspirePluTransformator.class.getResource( "/hale/xplanGml-inspirePlu.halex" ).toString();
+
+    }
+
+    @Override
+    public Path transformToPlu( Path xPlanGml )
+                            throws TransformationException {
+        try {
+            Path targetFile = Files.createTempFile( "inspirePlu", ".xml" );
+            String command = buildCommand( xPlanGml, targetFile );
+            LOG.info( "Execute the following command to transform the plan to INSPIRE PLU: {}", command );
+            Process process = Runtime.getRuntime().exec( command );
+            process.waitFor();
+            return targetFile;
+        } catch ( IOException | InterruptedException e ) {
+            LOG.error( "Could not transform to INSPIRE PLU", e );
+            throw new TransformationException( "Could not transform to INSPIRE PLU", e );
+        }
+    }
+
+    private String buildCommand( Path source, Path target ) {
+        StringBuilder sb = new StringBuilder();
+        sb.append( haleCli );
+        sb.append( " transform" );
+        sb.append( " -project " + haleProject );
+        sb.append( " -source " + source.toString() );
+        sb.append( " -target " + target.toString() );
+        sb.append( " -providerId eu.esdihumboldt.hale.io.gml.writer" );
+        return sb.toString();
+    }
+
+}
