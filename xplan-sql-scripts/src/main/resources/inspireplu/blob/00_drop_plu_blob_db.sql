1f700401e629fd6b2f37b3a85a85471672daf96b
Dirk Stenger <stenger@lat-lon.de>
2017-11-24 15:11:45 +0100

    Remove blob mode from inspireplu workspace and scripts

--- xplan-sql-scripts/src/main/resources/inspireplu/blob/00_drop_plu_blob_db.sql
+++
@@ -1,2 +0,0 @@
--- as user postgres
-DROP DATABASE plu_blob;
--- xplan-sql-scripts/src/main/resources/inspireplu/blob/02_create_plu_blob_db.sql
+++
@@ -1,8 +0,0 @@
--- as user postgres
-CREATE DATABASE plu_blob
-  WITH ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       CONNECTION LIMIT = -1;
-
-COMMENT ON DATABASE plu_blob
-  IS 'Planned Land Use - blob schema';
