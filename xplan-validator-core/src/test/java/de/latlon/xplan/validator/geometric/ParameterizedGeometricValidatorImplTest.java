ec2efb99e47e45c0ff7b0990594a7095933f4887
Lyn Elisa Goltz <goltz@lat-lon.de>
2018-08-29 15:01:18 +0200

    #49 - read testdata from csv file

--- xplan-validator-core/src/test/java/de/latlon/xplan/validator/geometric/ParameterizedGeometricValidatorImplTest.java
+++ xplan-validator-core/src/test/java/de/latlon/xplan/validator/geometric/ParameterizedGeometricValidatorImplTest.java
@@ -1,20 +1,22 @@
 package de.latlon.xplan.validator.geometric;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import javax.xml.stream.XMLStreamException;
 
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+
 import org.deegree.cs.exceptions.UnknownCRSException;
 import org.deegree.feature.types.AppSchema;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import de.latlon.xplan.ResourceAccessor;
 import de.latlon.xplan.commons.XPlanAde;
@@ -31,82 +33,53 @@ import de.latlon.xplan.validator.web.shared.ValidationOption;
 /**
  * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
  */
-@RunWith(Parameterized.class)
+@RunWith(JUnitParamsRunner.class)
 public class ParameterizedGeometricValidatorImplTest {
 
-    private ResourceUnderTest resourceUnderTest;
+    public static final String NULL = "null";
 
-    @Parameterized.Parameters
-    public static List<Object[]> data() {
-        List<Object[]> data = new ArrayList<>();
-        data.add( new Object[] { new ResourceUnderTest( "xplan2/BP2070.zip", true, 0, 12, 0, null, "4011000", null, 409 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan2/BP2135.zip", true, 0, 10, 0, "Bebauungsplan 2135",
-                                                        "4011000", "2135", 386 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan2/FPlan.zip", false, 109, 33, 106, "FPlan 1", "1234567",
-                                                        null, 3850 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan2/LA22.zip", false, 2, 213, 2, "Langenhorn 22",
-                                                        "20000000", "LA 22", 1978 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan2/LA67.zip", true, 0, 5, 0, "Bebauungsplan LA 67",
-                                                        "2000000", "LA 67", 165 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan3/BP2070.zip", true, 0, 12, 0, null, "4011000", null, 351 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan3/BP2135.zip", true, 0, 10, 0, "Bebauungsplan 2135",
-                                                        "4011000", "2135", 241 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan3/FPlan.zip", false, 109, 33, 106,
-                                                        "FPlan Bad Liebenwerda", "12062024", null, 3828 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan41/BP2070.zip", true, 0, 12, 0, null, "4011000", null,
-                                                        314 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan41/BP2135.zip", true, 0, 10, 0, "Bebauungsplan 2135",
-                                                        "4011000", "2135", 241 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan41/Demo.zip", true, 0, 2, 0, "BPlan Demo-Gemeinde",
-                                                        "1234567", null, 20 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan41/Eidelstedt_4_V4.zip", true, 0, 8, 0, "Eidelstedt 4",
-                                                        "02000000", null, 56 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan41/FPlan.zip", false, 109, 31, 106,
-                                                        "FPlan Bad Liebenwerda", "12062024", null, 3602 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan41/LA22.zip", false, 4, 214, 4, "Bebauungsplan LA 22",
-                                                        "02000000", "LA 22", 1349 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan41/LA67.zip", true, 0, 5, 0, "Bebauungsplan LA 22",
-                                                        "1234567", "LA 22", 146 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan41/V4_1_ID_103.zip", true, 0, 0, 0, "\"Heideweg\"",
-                                                        "12062425", "Nr.1", 500 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan40/V4_1_ID_66.zip", true, 0, 0, 0, "\"Weinbergstrasse\"",
-                                                        "12062425", "Nr. 2", 545 ) } );
-        data.add( new Object[] { new ResourceUnderTest( "xplan41/PlanWithComplexCurve.zip", true, 0, 8, 0, "test5", "",
-                                                        null, 1 ) } );
-        return data;
-    }
-
-    public ParameterizedGeometricValidatorImplTest( ResourceUnderTest resourceUnderTest ) {
-        this.resourceUnderTest = resourceUnderTest;
-    }
-
+    @FileParameters("src/test/resources/de/latlon/xplan/validator/geometric/geometricValidatorImplTest-validateGeometry-input.csv")
     @Test
-    public void testValidateGeometry()
+    public void testValidateGeometry( String testResource, boolean expectedValidationResult,
+                                      int expectedNumberOfErrors, int expectedNumberOfWarnings,
+                                      int expectedNumberOBadGeometries )
                             throws Exception {
-
-        XPlanArchive archive = getTestArchive( resourceUnderTest.testReource );
+        XPlanArchive archive = getTestArchive( testResource );
         ValidatorResult report = validateGeometryAndReturnReport( archive, Collections.emptyList() );
         GeometricValidatorResult geometricReport = (GeometricValidatorResult) report;
         int numberOfErrors = geometricReport.getErrors().size();
         int numberOfWarnings = geometricReport.getWarnings().size();
         int numberOfBadGeometries = geometricReport.getBadGeometries().size();
 
-        assertThat( report.isValid(), is( resourceUnderTest.expectedValidationResult ) );
-        assertThat( numberOfErrors, is( resourceUnderTest.expectedNumberOfErrors ) );
-        assertThat( numberOfWarnings, is( resourceUnderTest.expectedNumberOfWarnings ) );
-        assertThat( numberOfBadGeometries, is( resourceUnderTest.expectedNumberOBadGeometries ) );
+        assertThat( report.isValid(), is( expectedValidationResult ) );
+        assertThat( numberOfErrors, is( expectedNumberOfErrors ) );
+        assertThat( numberOfWarnings, is( expectedNumberOfWarnings ) );
+        assertThat( numberOfBadGeometries, is( expectedNumberOBadGeometries ) );
     }
 
+    @FileParameters("src/test/resources/de/latlon/xplan/validator/geometric/geometricValidatorImplTest-retrieveFeatures-input.csv")
     @Test
-    public void testRetrieveGeometricallyValidXPlanFeatures()
+    public void testRetrieveGeometricallyValidXPlanFeatures( String testResource, String expectedPlanName,
+                                                             String expectedPlanGz, String expectedPlanNumber,
+                                                             int expectedNumberOfFeatures )
                             throws Exception {
-        XPlanArchive archive = getTestArchive( resourceUnderTest.testReource );
+        XPlanArchive archive = getTestArchive( testResource );
         XPlanFeatureCollection fc = readFeaturesAndAssertGeometryValidity( archive );
-        if ( resourceUnderTest.expectedPlanName != null )
-            assertThat( fc.getPlanName(), is( resourceUnderTest.expectedPlanName ) );
-        assertThat( fc.getPlanGkz(), is( resourceUnderTest.expectedPlanGz ) );
-        assertThat( fc.getPlanNummer(), is( resourceUnderTest.expectedPlanNumber ) );
-        assertThat( fc.getFeatures().size(), is( resourceUnderTest.expectedNumberOfFeatures ) );
+        // planName
+        if ( !NULL.equals( expectedPlanName ) )
+            assertThat( fc.getPlanName(), is( expectedPlanName ) );
+        // planGz
+        if ( NULL.equals( expectedPlanGz ) )
+            assertThat( fc.getPlanGkz(), is( nullValue() ) );
+        else
+            assertThat( fc.getPlanGkz(), is( expectedPlanGz ) );
+        // planNummer
+        if ( NULL.equals( expectedPlanNumber ) )
+            assertThat( fc.getPlanNummer(), is( nullValue() ) );
+        else
+            assertThat( fc.getPlanNummer(), is( expectedPlanNumber ) );
+        // Features size
+        assertThat( fc.getFeatures().size(), is( expectedNumberOfFeatures ) );
     }
 
     private ValidatorResult validateGeometryAndReturnReport( XPlanArchive archive, List<ValidationOption> voOptions )
@@ -130,41 +103,6 @@ public class ParameterizedGeometricValidatorImplTest {
                             throws IOException {
         XPlanArchiveCreator archiveCreator = new XPlanArchiveCreator();
         return archiveCreator.createXPlanArchive( name, ResourceAccessor.readResourceStream( name ) );
-    }
-
-    private static class ResourceUnderTest {
-
-        String testReource;
-
-        boolean expectedValidationResult;
-
-        int expectedNumberOfErrors;
-
-        int expectedNumberOfWarnings;
-
-        int expectedNumberOBadGeometries;
-
-        private String expectedPlanName;
-
-        String expectedPlanGz;
-
-        String expectedPlanNumber;
-
-        int expectedNumberOfFeatures;
-
-        ResourceUnderTest( String testReource, boolean expectedValidationResult, int expectedNumberOfErrors,
-                           int expectedNumberOfWarnings, int expectedNumberOBadGeometries, String expectedPlanName,
-                           String expectedPlanGz, String expectedPlanNumber, int expectedNumberOfFeatures ) {
-            this.testReource = testReource;
-            this.expectedValidationResult = expectedValidationResult;
-            this.expectedNumberOfErrors = expectedNumberOfErrors;
-            this.expectedNumberOfWarnings = expectedNumberOfWarnings;
-            this.expectedNumberOBadGeometries = expectedNumberOBadGeometries;
-            this.expectedPlanName = expectedPlanName;
-            this.expectedPlanGz = expectedPlanGz;
-            this.expectedPlanNumber = expectedPlanNumber;
-            this.expectedNumberOfFeatures = expectedNumberOfFeatures;
-        }
     }
 
 }
