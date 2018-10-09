9677540cc7e61605218e21d6999556eba580551b
Lyn Elisa Goltz <goltz@lat-lon.de>
2018-10-09 14:57:22 +0200

    #49 - fixed mapping (wrong feature type)

--- xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
+++ xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
@@ -13184,6 +13184,15 @@ CREATE TABLE xplansynarchive.xplan_rp_naturlandschaft (
     xplan_gehoertnachrichtlichzubereich text,
     xplan_wirddargestelltdurch text,
     xplan_gehoertzurp_bereich text,
+    xplan_kuestenmeer text,
+    xplan_bedeutsamkeit text,
+    xplan_bedeutsamkeitcode text,
+    xplan_istzweckbindung text,
+    xplan_flaechenschluss text,
+    xplan_flussrichtung text,
+    xplan_imverbund text,
+    xplan_typ text,
+    xplan_typcode text,
     CONSTRAINT xplan_rp_naturlandschaft_pkey PRIMARY KEY (attr_gml_id)
 );
 SELECT ADDGEOMETRYCOLUMN('xplansynarchive', 'xplan_rp_naturlandschaft','xplan_symbolposition','-1','GEOMETRY', 2);
@@ -13232,15 +13241,6 @@ CREATE TABLE xplansynarchive.xplan_rp_naturschutzrechtlichesschutzgebiet (
     xplan_gehoertzurp_bereich text,
     xplan_besonderezweckbestimmungnsg_nsm text,
     xplan_besonderezweckbestimmungnsg_nsmcode text,
-    xplan_kuestenmeer text,
-    xplan_bedeutsamkeit text,
-    xplan_bedeutsamkeitcode text,
-    xplan_istzweckbindung text,
-    xplan_flaechenschluss text,
-    xplan_flussrichtung text,
-    xplan_imverbund text,
-    xplan_typ text,
-    xplan_typcode text,
     CONSTRAINT xplan_rp_naturschutzrechtlichesschutzgebiet_pkey PRIMARY KEY (attr_gml_id)
 );
 SELECT ADDGEOMETRYCOLUMN('xplansynarchive', 'xplan_rp_naturschutzrechtlichesschutzgebiet','xplan_symbolposition','-1','GEOMETRY', 2);
