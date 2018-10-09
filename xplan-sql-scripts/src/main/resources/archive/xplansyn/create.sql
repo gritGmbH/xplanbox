93b6a8bad988e42136d84a7842917c58e44abf84
Lyn Elisa Goltz <goltz@lat-lon.de>
2018-10-09 14:18:12 +0200

    #49 - rules for RP_GenerischesObjekt

--- xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
+++ xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
@@ -11038,6 +11038,16 @@ CREATE TABLE xplansynarchive.xplan_rp_generischesobjekt (
     xplan_gehoertnachrichtlichzubereich text,
     xplan_wirddargestelltdurch text,
     xplan_gehoertzurp_bereich text,
+    xplan_gebietstyp text,
+    xplan_gebietstypcode text,
+    xplan_kuestenmeer text,
+    xplan_bedeutsamkeit text,
+    xplan_bedeutsamkeitcode text,
+    xplan_istzweckbindung text,
+    xplan_flaechenschluss text,
+    xplan_flussrichtung text,
+    xplan_typ text,
+    xplan_typcode text,
     CONSTRAINT xplan_rp_generischesobjekt_pkey PRIMARY KEY (attr_gml_id)
 );
 SELECT ADDGEOMETRYCOLUMN('xplansynarchive', 'xplan_rp_generischesobjekt','xplan_symbolposition','-1','GEOMETRY', 2);
