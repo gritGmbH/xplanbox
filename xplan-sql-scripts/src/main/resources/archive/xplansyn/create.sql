a748c8e1527efb0d2a00aa0620ed5f325eaedc11
Lyn Elisa Goltz <goltz@lat-lon.de>
2018-10-09 13:01:07 +0200

    #49 - rules for RP_Bodenschutz

--- xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
+++ xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
@@ -10675,6 +10675,15 @@ CREATE TABLE xplansynarchive.xplan_rp_bodenschutz (
     xplan_gehoertzurp_bereich text,
     xplan_zweckbestimmungbs_nsm text,
     xplan_zweckbestimmungbs_nsmcode text,
+    xplan_kuestenmeer text,
+    xplan_bedeutsamkeit text,
+    xplan_bedeutsamkeitcode text,
+    xplan_istzweckbindung text,
+    xplan_flaechenschluss text,
+    xplan_flussrichtung text,
+    xplan_imverbund text,
+    xplan_typ text,
+    xplan_typcode text,
     CONSTRAINT xplan_rp_bodenschutz_pkey PRIMARY KEY (attr_gml_id)
 );
 SELECT ADDGEOMETRYCOLUMN('xplansynarchive', 'xplan_rp_bodenschutz','xplan_symbolposition','-1','GEOMETRY', 2);
