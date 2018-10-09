873a7e1d602963f67142766c6e2fb477c6bc0ce5
Lyn Elisa Goltz <goltz@lat-lon.de>
2018-10-09 13:45:48 +0200

    #49 - rules for RP_Entsorgung

--- xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
+++ xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
@@ -10792,6 +10792,19 @@ CREATE TABLE xplansynarchive.xplan_rp_entsorgung (
     xplan_gehoertnachrichtlichzubereich text,
     xplan_wirddargestelltdurch text,
     xplan_gehoertzurp_bereich text,
+    xplan_kuestenmeer text,
+    xplan_bedeutsamkeit text,
+    xplan_bedeutsamkeitcode text,
+    xplan_istzweckbindung text,
+    xplan_flaechenschluss text,
+    xplan_flussrichtung text,
+    xplan_typae text,
+    xplan_typaecode text,
+    xplan_abfalltyp text,
+    xplan_abfalltypcode text,
+    xplan_typaw text,
+    xplan_typawcode text,
+    xplan_istaufschuettungablagerung text,
     CONSTRAINT xplan_rp_entsorgung_pkey PRIMARY KEY (attr_gml_id)
 );
 SELECT ADDGEOMETRYCOLUMN('xplansynarchive', 'xplan_rp_entsorgung','xplan_symbolposition','-1','GEOMETRY', 2);
