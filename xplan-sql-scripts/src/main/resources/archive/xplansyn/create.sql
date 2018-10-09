44df25aa809223c4d13f76f557cf7d6fee902e6c
Lyn Elisa Goltz <goltz@lat-lon.de>
2018-10-09 15:23:33 +0200

    #49 - rules for RP_Sperrgebiet

--- xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
+++ xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
@@ -13705,6 +13705,16 @@ CREATE TABLE xplansynarchive.xplan_rp_sperrgebiet (
     xplan_gehoertzurp_bereich text,
     xplan_sperrgebietTyp_nsm text,
     xplan_sperrgebietTyp_nsmcode text,
+    xplan_gebietstyp text,
+    xplan_gebietstypcode text,
+    xplan_kuestenmeer text,
+    xplan_bedeutsamkeit text,
+    xplan_bedeutsamkeitcode text,
+    xplan_istzweckbindung text,
+    xplan_flaechenschluss text,
+    xplan_flussrichtung text,
+    xplan_typ text,
+    xplan_typcode text,
     CONSTRAINT xplan_rp_sperrgebiet_pkey PRIMARY KEY (attr_gml_id)
 );
 SELECT ADDGEOMETRYCOLUMN('xplansynarchive', 'xplan_rp_sperrgebiet','xplan_position','-1','GEOMETRY', 2);
