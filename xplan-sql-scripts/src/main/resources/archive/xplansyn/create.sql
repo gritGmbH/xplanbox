13495cf2f5a676db03022ea51b047247cdf277a3
Lyn Elisa Goltz <goltz@lat-lon.de>
2018-10-10 08:04:04 +0200

    #49 - added RP_Erholung

--- xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
+++ xplan-sql-scripts/src/main/resources/archive/xplansyn/create.sql
@@ -10861,6 +10861,60 @@ SELECT ADDGEOMETRYCOLUMN('xplansynarchive', 'xplan_rp_entsorgung','xplan_symbolp
 CREATE INDEX spatial_idx_305 ON xplan_rp_entsorgung USING GIST ( xplan_symbolposition  );
 SELECT ADDGEOMETRYCOLUMN('xplansynarchive', 'xplan_rp_entsorgung','xplan_position','-1','GEOMETRY', 2);
 CREATE INDEX spatial_idx_306 ON xplan_rp_entsorgung USING GIST ( xplan_position  );
+CREATE TABLE xplansynarchive.xplan_rp_erholung (
+    attr_gml_id text,
+    xplan_gmlid text,
+    xplan_gmlname text,
+    xplan_gmldescription text,
+    xplan_xpversion text,
+    xplan_xpplantype text,
+    xplan_xpplanname text,
+    xplan_mgr_planid integer,
+    xplan_gueltigkeitBeginn timestamp,
+    xplan_gueltigkeitEnde timestamp,
+    xplan_wmsSortDate date,
+    xplan_uuid text,
+    xplan_text text,
+    xplan_rechtsstand text,
+    xplan_rechtsstandcode text,
+    xplan_gesetzlichegrundlage text,
+    xplan_gesetzlichegrundlagecode text,
+    xplan_gliederung1 text,
+    xplan_gliederung2 text,
+    xplan_ebene integer,
+    xplan_hatgenerattribut text,
+    xplan_hoehenangabe text,
+    xplan_externereferenz text,
+    xplan_wirddargestelltdurch text,
+    xplan_refbegruendunginhalt text,
+    xplan_startbedingung text,
+    xplan_endebedingung text,
+    xplan_rechtscharakter text,
+    xplan_rechtscharaktercode text,
+    xplan_konkretisierung text,
+    xplan_gebietstyp text,
+    xplan_gebietstypcode text,
+    xplan_kuestenmeer text,
+    xplan_bedeutsamkeit text,
+    xplan_bedeutsamkeitcode text,
+    xplan_istzweckbindung text,
+    xplan_reftextinhalt text,
+    xplan_flaechenschluss text,
+    xplan_flussrichtung text,
+    xplan_istausgleichsgebiet text,
+    xplan_imverbund text,
+    xplan_typerholungcode text,
+    xplan_typerholungcode text,
+    xplan_typtourismus text,
+    xplan_typtourismuscode text,
+    xplan_besonderertyp text,
+    xplan_besonderertypcode text,
+    CONSTRAINT xplan_rp_verkehr_pkey PRIMARY KEY (attr_gml_id)
+);
+SELECT ADDGEOMETRYCOLUMN('xplansynarchive', 'xplan_rp_erholung','xplan_symbolposition','-1','GEOMETRY', 2);
+CREATE INDEX spatial_idx_392 ON xplan_rp_erholung USING GIST ( xplan_symbolposition  );
+SELECT ADDGEOMETRYCOLUMN('xplansynarchive', 'xplan_rp_erholung','xplan_position','-1','GEOMETRY', 2);
+CREATE INDEX spatial_idx_393 ON xplan_rp_erholung USING GIST ( xplan_position  );
 CREATE TABLE xplansynarchive.xplan_rp_entwicklungsschwerpunkte (
     attr_gml_id text,
     xplan_gmlid text,
