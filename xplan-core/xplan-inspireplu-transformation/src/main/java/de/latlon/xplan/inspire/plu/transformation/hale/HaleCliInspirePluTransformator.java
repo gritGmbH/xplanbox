6e9b1a2859cd5ad9187eeda1fa13fbf307c657fb
Lyn Elisa Goltz <goltz@lat-lon.de>
2018-11-29 08:56:36 +0100

    #70 - included hale project (XPlan GML 5.1 to INSPPIRE PLU)

--- xplan-core/xplan-inspireplu-transformation/src/main/java/de/latlon/xplan/inspire/plu/transformation/hale/HaleCliInspirePluTransformator.java
+++ xplan-core/xplan-inspireplu-transformation/src/main/java/de/latlon/xplan/inspire/plu/transformation/hale/HaleCliInspirePluTransformator.java
@@ -9,6 +9,15 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static de.latlon.xplan.commons.XPlanVersion.XPLAN_41;
+import static de.latlon.xplan.commons.XPlanVersion.XPLAN_50;
+import static de.latlon.xplan.commons.XPlanVersion.XPLAN_51;
 
 /**
  * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
@@ -17,9 +26,11 @@ public class HaleCliInspirePluTransformator implements InspirePluTransformator {
 
     private static final Logger LOG = LoggerFactory.getLogger( HaleCliInspirePluTransformator.class );
 
-    public static final String PATH_TO_HALE_PROJECT_41 = "xplan41/xplanGml41-inspirePlu.halex";
-
-    public static final String PATH_TO_HALE_PROJECT_50 = "xplan50/xplanGml50-inspirePlu.halex";
+    private static final Map<XPlanVersion, String> VERSION_TO_HALEPROJECT = Collections.unmodifiableMap(
+                    Stream.of( new SimpleEntry<>( XPLAN_41, "xplan41/xplanGml41-inspirePlu.halex" ),
+                               new SimpleEntry<>( XPLAN_50, "xplan50/xplanGml50-inspirePlu.halex" ),
+                               new SimpleEntry<>( XPLAN_51, "xplan51/xplanGml51-inspirePlu.halex" ) ).collect(
+                                    Collectors.toMap( ( e ) -> e.getKey(), ( e ) -> e.getValue() ) ) );
 
     private final String haleCli;
 
@@ -60,22 +71,16 @@ public class HaleCliInspirePluTransformator implements InspirePluTransformator {
 
     private String findHaleProject( XPlanVersion xPlanVersion )
                     throws TransformationException {
-        switch ( xPlanVersion ) {
-        case XPLAN_41:
-            return resolvePathToHale( xPlanVersion, PATH_TO_HALE_PROJECT_41 );
-        case XPLAN_50:
-            return resolvePathToHale( xPlanVersion, PATH_TO_HALE_PROJECT_50 );
-        default:
-            throw new TransformationException(
-                            "Could not find HALE project from XPlan GML " + xPlanVersion.name() + " to INSPIRE PLU" );
-        }
+        String pathToHaleProject = VERSION_TO_HALEPROJECT.get( xPlanVersion );
+        if ( pathToHaleProject != null )
+            return resolvePathToHale( xPlanVersion, pathToHaleProject );
+        throw new TransformationException(
+                        "Could not find HALE project from XPlan GML " + xPlanVersion.name() + " to INSPIRE PLU" );
     }
 
     private String resolvePathToHale( XPlanVersion xPlanVersion, String haleProjectPath )
                     throws TransformationException {
-        System.out.println( haleProjectPath );
         Path haleProject = haleProjectDirectory.resolve( haleProjectPath );
-        System.out.println( haleProject );
         if ( Files.exists( haleProject ) )
             return haleProject.toString();
         throw new TransformationException(
