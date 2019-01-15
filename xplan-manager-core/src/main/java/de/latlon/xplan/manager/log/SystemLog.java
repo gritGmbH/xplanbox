d5a5ab0d7021a8dcc3981a1b1cf43c5d1ee93531
Lyn Elisa Goltz <goltz@lat-lon.de>
2019-01-15 18:15:12 +0100

    #92 - log system information during startup of XPlanManagerWeb

--- xplan-manager-core/src/main/java/de/latlon/xplan/manager/log/SystemLog.java
+++ xplan-manager-core/src/main/java/de/latlon/xplan/manager/log/SystemLog.java
@@ -1,31 +1,28 @@
 //$HeadURL$
-package de.latlon.xplan.manager.cli;
-
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
-
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerFactory;
+package de.latlon.xplan.manager.log;
 
 import org.apache.xalan.Version;
 import org.apache.xalan.xslt.EnvironmentCheck;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.latlon.xplan.commons.XPlanFeatureCollection;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
 
 /**
  * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz</a>
  * @author last edited by: $Author: lyn $
- * 
  * @version $Revision: $, $Date: $
  */
 public class SystemLog {
 
-    private static final Logger LOG = LoggerFactory.getLogger( XPlanFeatureCollection.class );
+    private static final Logger LOG = LoggerFactory.getLogger( SystemLog.class );
 
     /**
      * Logs important stuff about the system, like java version, operating system, xalan and xerces version.
@@ -36,8 +33,8 @@ public class SystemLog {
         LOG.info( "System info" );
         LOG.info( "--------------------------------------------------------------------------------" );
         LOG.info( "" );
-        LOG.info( "- java version       " + System.getProperty( "java.version" ) + " ("
-                  + System.getProperty( "java.vendor" ) + ")" );
+        LOG.info( "- java version       " + System.getProperty( "java.version" ) + " (" + System.getProperty(
+                        "java.vendor" ) + ")" );
         LOG.info( "- operating system   " + System.getProperty( "os.name" ) + " (" + System.getProperty( "os.version" )
                   + ", " + System.getProperty( "os.arch" ) + ")" );
         LOG.info( "- system encoding    " + Charset.defaultCharset().displayName() );
@@ -51,8 +48,10 @@ public class SystemLog {
         LOG.info( "    - product: " + Version.getProduct() );
         LOG.info( "    - release version: " + Version.getReleaseVersionNum() );
         LOG.info( "    - version: " + Version.getVersion() );
-        PrintWriter pw = new PrintWriter( System.out );
+        StringWriter envCheck = new StringWriter();
+        PrintWriter pw = new PrintWriter( envCheck, true );
         ( new EnvironmentCheck() ).checkEnvironment( pw );
+        LOG.info( envCheck.toString() );
         LOG.info( "- xerces environment " );
         LOG.info( "    - version: " + org.apache.xerces.impl.Version.getVersion() );
         logTransformer();
@@ -63,8 +62,8 @@ public class SystemLog {
     private static void logTransformer() {
         try {
             LOG.info( "- transformer " );
-            LOG.info( "    - system property (-Djava.xml.transform.TransformerFactory): '"
-                      + System.getProperty( "java.xml.transform.TransformerFactory" ) + "'" );
+            LOG.info( "    - system property (-Djava.xml.transform.TransformerFactory): '" + System.getProperty(
+                            "java.xml.transform.TransformerFactory" ) + "'" );
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             LOG.info( "    - factory " + transformerFactory.getClass() );
             Transformer transformer = transformerFactory.newTransformer();
