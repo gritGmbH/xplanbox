3fa0477db57aa2551efe3fff35f8636374b1ed77
Lyn Elisa Goltz <goltz@lat-lon.de>
2019-04-02 12:12:48 +0200

    #52 - added schema

--- xplan-cli/xplan-transform-cli/src/main/resources/sql/02_create_trigger-functions.sql
+++ xplan-cli/xplan-transform-cli/src/main/resources/sql/02_create_trigger-functions.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION updateTransformToolPlanPlanLog()
+CREATE OR REPLACE FUNCTION xplanmgr.updateTransformToolPlanPlanLog()
   RETURNS trigger AS
 $$
 DECLARE
--- xplan-cli/xplan-transform-cli/src/main/resources/sql/03_create_trigger.sql
+++ xplan-cli/xplan-transform-cli/src/main/resources/sql/03_create_trigger.sql
@@ -1,7 +1,7 @@
 CREATE TRIGGER insert_xplanmgr_plans_transform_tool AFTER INSERT OR UPDATE ON xplanmgr.plans
   FOR EACH ROW
-    EXECUTE PROCEDURE updateTransformToolPlanPlanLog();
+    EXECUTE PROCEDURE xplanmgr.updateTransformToolPlanPlanLog();
 
 CREATE TRIGGER delete_xplanmgr_plans_transform_tool BEFORE DELETE ON xplanmgr.plans
   FOR EACH ROW
-    EXECUTE PROCEDURE updateTransformToolPlanPlanLog();
+    EXECUTE PROCEDURE xplanmgr.updateTransformToolPlanPlanLog();
