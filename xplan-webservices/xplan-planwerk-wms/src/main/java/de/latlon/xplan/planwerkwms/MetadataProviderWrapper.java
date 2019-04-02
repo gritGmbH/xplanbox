6e044d5e8e5c15ad270b5c87f73a580f756002f2
Lyn Elisa Goltz <goltz@lat-lon.de>
2019-04-02 22:29:00 +0200

    #104 - set service metadata url in extended capabilities

--- xplan-webservices/xplan-planwerk-wms/src/main/java/de/latlon/xplan/planwerkwms/MetadataProviderWrapper.java
+++ xplan-webservices/xplan-planwerk-wms/src/main/java/de/latlon/xplan/planwerkwms/MetadataProviderWrapper.java
@@ -1,6 +1,8 @@
 package de.latlon.xplan.planwerkwms;
 
 import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.util.XMLStreamWriterFilterBase;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.deegree.commons.ows.metadata.DatasetMetadata;
 import org.deegree.commons.ows.metadata.MetadataUrl;
 import org.deegree.commons.ows.metadata.ServiceIdentification;
@@ -11,19 +13,34 @@ import org.deegree.services.metadata.OWSMetadataProvider;
 import org.deegree.services.planwerk.jaxb.Planwerk;
 import org.deegree.workspace.Resource;
 import org.deegree.workspace.ResourceMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.xml.namespace.QName;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.util.Collections.singletonList;
+import static org.apache.axiom.om.util.AXIOMUtil.stringToOM;
+import static org.deegree.commons.xml.stax.XMLStreamUtils.closeQuietly;
+import static org.deegree.commons.xml.stax.XMLStreamUtils.copy;
 
 /**
  * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
  */
 public class MetadataProviderWrapper implements OWSMetadataProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger( MetadataProviderWrapper.class );
+
+    private static final QName EXTENDED_CAPABILITIES = new QName( "http://inspire.ec.europa.eu/schemas/inspire_vs/1.0",
+                                                                  "ExtendedCapabilities" );
 
     private final OWSMetadataProvider wrappedProvider;
 
@@ -55,7 +72,14 @@ public class MetadataProviderWrapper implements OWSMetadataProvider {
 
     @Override
     public Map<String, List<OMElement>> getExtendedCapabilities() {
-        return wrappedProvider.getExtendedCapabilities();
+        Map<String, List<OMElement>> extendedCapabilities = wrappedProvider.getExtendedCapabilities();
+        List<String> serviceMetadataUrls = planwerk.getServiceMetadataUrl();
+        if ( extendedCapabilities != null && !serviceMetadataUrls.isEmpty() ) {
+            if ( serviceMetadataUrls.size() > 2 )
+                LOG.warn( "Currently only one service metadata is allowd" );
+            return createCopyOfExtendedCapabilities( extendedCapabilities, serviceMetadataUrls );
+        }
+        return extendedCapabilities;
     }
 
     @Override
@@ -110,6 +134,102 @@ public class MetadataProviderWrapper implements OWSMetadataProvider {
         if ( wmsTitle == null || wmsTitle.isEmpty() )
             return planwerk.getName();
         return wmsTitle.stream().collect( Collectors.joining( ", " ) );
+    }
+
+    private Map<String, List<OMElement>> createCopyOfExtendedCapabilities(
+                    Map<String, List<OMElement>> extendedCapabilities, List<String> serviceMetadataUrls ) {
+        Map<String, List<OMElement>> extendedCapabilitiesToReturn = new HashMap<>();
+        for ( Map.Entry<String, List<OMElement>> extendedCapabilitiesEntry : extendedCapabilities.entrySet() ) {
+            List<OMElement> copyOfElements = new ArrayList<>();
+            for ( OMElement xml : extendedCapabilitiesEntry.getValue() ) {
+                if ( EXTENDED_CAPABILITIES.equals( xml.getQName() ) ) {
+                    OMElement copyWithNewServiceMetadataUrl = createCopy( xml, serviceMetadataUrls.get( 0 ) );
+                    copyOfElements.add( copyWithNewServiceMetadataUrl );
+                } else {
+                    copyOfElements.add( xml );
+                }
+            }
+            extendedCapabilitiesToReturn.put( extendedCapabilitiesEntry.getKey(), copyOfElements );
+        }
+        return extendedCapabilitiesToReturn;
+    }
+
+    private OMElement createCopy( OMElement xml, String serviceMetadataUrl ) {
+        XMLStreamReader xmlStreamReader = null;
+        XMLStreamWriter writer = null;
+        try {
+            xmlStreamReader = xml.getXMLStreamReader();
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            writer = XMLOutputFactory.newFactory().createXMLStreamWriter( bos );
+            XMLStreamWriterFilterBase filter = new ServiceMetadataUrlFilter( serviceMetadataUrl );
+            filter.setDelegate( writer );
+            copy( filter, xmlStreamReader );
+            writer.close();
+
+            String xmlFragment = new String( bos.toByteArray() );
+            return stringToOM( xmlFragment );
+        } catch ( XMLStreamException e ) {
+            LOG.warn( "Could not pass service metadata url to extended capabilities" );
+            return xml;
+        } finally {
+            closeQuietly( xmlStreamReader );
+            closeQietly( writer );
+        }
+    }
+
+    private void closeQietly( XMLStreamWriter writer ) {
+        if ( writer != null ) {
+            try {
+                writer.close();
+            } catch ( XMLStreamException e ) {
+            }
+        }
+    }
+
+    private class ServiceMetadataUrlFilter extends XMLStreamWriterFilterBase {
+
+        private final String serviceMetadataUrl;
+
+        private String path = "";
+
+        ServiceMetadataUrlFilter( String serviceMetadataUrl ) {
+            this.serviceMetadataUrl = serviceMetadataUrl;
+        }
+
+        @Override
+        public void writeStartElement( String prefix, String localName, String namespaceURI )
+                        throws XMLStreamException {
+            path += '/' + localName;
+            super.writeStartElement( prefix, localName, namespaceURI );
+        }
+
+        @Override
+        public void writeStartElement( String namespaceURI, String localName )
+                        throws XMLStreamException {
+            path += '/' + localName;
+            super.writeStartElement( namespaceURI, localName );
+        }
+
+        @Override
+        public void writeStartElement( String localName )
+                        throws XMLStreamException {
+            path += '/' + localName;
+            super.writeStartElement( localName );
+        }
+
+        @Override
+        public void writeEndElement()
+                        throws XMLStreamException {
+            path = path.substring( 0, path.lastIndexOf( "/" ) );
+            super.writeEndElement();
+        }
+
+        @Override
+        protected String xmlData( String s ) {
+            if ( path.endsWith( "MetadataUrl/URL" ) )
+                return serviceMetadataUrl;
+            return s;
+        }
     }
 
 }
