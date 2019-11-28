d9e1c0fecb071b62387f26580fbf2b7eaf7f93fd
Dirk Stenger <stenger@lat-lon.de>
2019-11-28 16:49:50 +0100

    Add xplan50 and xplan51 to create.sql and drop.sql in xplan-manager-workspace

--- xplan-workspaces/xplan-manager-workspace/src/main/workspace/sql/create.sql
+++ xplan-workspaces/xplan-manager-workspace/src/main/workspace/sql/create.sql
@@ -3,11 +3,15 @@
 \i fix/xplan40/create.sql
 \i fix/xplan41/create.sql
 \i fix/xplan41nsm/create.sql
+\i fix/xplan50/create.sql
+\i fix/xplan51/create.sql
 \i fix/xplansyn/create.sql
 \i pre/xplan2/create.sql
 \i pre/xplan3/create.sql
 \i pre/xplan40/create.sql
 \i pre/xplan41/create.sql
+\i pre/xplan50/create.sql
+\i pre/xplan51/create.sql
 \i pre/xplan41nsm/create.sql
 \i pre/xplansyn/create.sql
 \i archive/xplan2/create.sql
@@ -15,5 +19,7 @@
 \i archive/xplan40/create.sql
 \i archive/xplan41/create.sql
 \i archive/xplan41nsm/create.sql
+\i archive/xplan50/create.sql
+\i archive/xplan51/create.sql
 \i archive/xplansyn/create.sql
 \i xplanmgr/create.sql
