1c228cb887991203933cb0ef208438c5cbcacb6f
Torsten Friebe <torsten.friebe@gmail.com>
2020-08-31 16:32:54 +0200

    XPLANBOX-316 sets application path to /xmanager/api/v1 and /xvalidator/api/v1

--- xplan-api/xplan-api-manager/src/main/java/de/latlon/xplanbox/api/manager/RestApplication.java
+++ xplan-api/xplan-api-manager/src/main/java/de/latlon/xplanbox/api/manager/RestApplication.java
@@ -3,6 +3,15 @@ package de.latlon.xplanbox.api.manager;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ * Application configuration for XPlanManager REST API.
+ * Example mapping for proxy mapping:
+ * http://xplanbox.lat-lon.de/xmanager/api/vi/ -> http://host:8080/xplan-api-validator/xmanager/api/v1/
+ * Public address: http://xplanbox.lat-lon.de/xmanager/
+ * Internal address: http://host:8080/xplan-api-validator/xmanager/
+ *
+ * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
+ */
 @ApplicationPath("/xmanager/api/v1")
 public class RestApplication extends Application {
 
--- xplan-api/xplan-api-validator/src/main/java/de/latlon/xplanbox/api/validator/v1/XPlanApiValidator.java
+++ xplan-api/xplan-api-validator/src/main/java/de/latlon/xplanbox/api/validator/v1/XPlanApiValidator.java
@@ -6,9 +6,14 @@ import org.glassfish.jersey.server.ResourceConfig;
 import javax.ws.rs.ApplicationPath;
 
 /**
+ * Application configuration for XPlanValidator REST API.
+ * Example mapping for proxy mapping:
+ * http://xplanbox.lat-lon.de/xvalidator/api/vi/ -> http://host:8080/xplan-api-validator/xvalidator/api/v1/
+ * Public address: http://xplanbox.lat-lon.de/xvalidator/
+ * Internal address: http://host:8080/xplan-api-validator/xvalidator/
  * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
  */
-@ApplicationPath("/")
+@ApplicationPath("/xvalidator/api/v1")
 public class XPlanApiValidator extends ResourceConfig {
 
     public XPlanApiValidator() {
