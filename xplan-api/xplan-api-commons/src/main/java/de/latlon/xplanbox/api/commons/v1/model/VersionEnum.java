f662ad2ba33274779ca237f5dd0668a5412ada47
Lyn Elisa Goltz <goltz@lat-lon.de>
2020-10-05 07:10:15 +0200

    XPLANBOX-348 - renamed VersionEnum entries

--- xplan-api/xplan-api-commons/src/main/java/de/latlon/xplanbox/api/commons/v1/model/VersionEnum.java
+++ xplan-api/xplan-api-commons/src/main/java/de/latlon/xplanbox/api/commons/v1/model/VersionEnum.java
@@ -5,9 +5,17 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum VersionEnum {
 
-    _3( String.valueOf( "XPLAN_3" ) ), _40( String.valueOf( "XPLAN_40" ) ), _41( String.valueOf( "XPLAN_41" ) ), _50(
-                            String.valueOf( "XPLAN_50" ) ), _51( String.valueOf( "XPLAN_51" ) ), _52(
-                            String.valueOf( "XPLAN_52" ) );
+    XPLAN_3( String.valueOf( "XPLAN_3" ) ),
+
+    XPLAN_40( String.valueOf( "XPLAN_40" ) ),
+
+    XPLAN_41( String.valueOf( "XPLAN_41" ) ),
+
+    XPLAN_50( String.valueOf( "XPLAN_50" ) ),
+
+    XPLAN_51( String.valueOf( "XPLAN_51" ) ),
+
+    XPLAN_52( String.valueOf( "XPLAN_52" ) );
 
     private String value;
 
