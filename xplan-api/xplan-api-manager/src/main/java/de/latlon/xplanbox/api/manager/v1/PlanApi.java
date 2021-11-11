d27475680ddc84542838b1ddc0c1e7c9cdd62ef0
Lyn Elisa Goltz <goltz@lat-lon.de>
2021-11-11 07:21:56 +0100

    XPLANBOX-284 - added pattern to X-Filename parameter

--- xplan-api/xplan-api-manager/src/main/java/de/latlon/xplanbox/api/manager/v1/PlanApi.java
+++ xplan-api/xplan-api-manager/src/main/java/de/latlon/xplanbox/api/manager/v1/PlanApi.java
@@ -105,8 +105,8 @@ public class PlanApi {
                             @Context
                                                     Request request, @Valid File body,
                             @HeaderParam("X-Filename")
-                            @Parameter(description = "Name of the file to be uploaded", example = "File names such as xplan.gml, xplan.xml, xplan.zip")
-                                                    String xFilename,
+                            @Parameter(description = "Name of the file to be uploaded", example = "File names such as xplan.gml, xplan.xml, xplan.zip", schema = @Schema(pattern = "^[A-Za-z0-9()_-]*$"))
+                                            String xFilename,
                             @QueryParam("skipSemantisch")
                             @DefaultValue("false")
                             @Parameter(description = "skip semantische Validierung")
