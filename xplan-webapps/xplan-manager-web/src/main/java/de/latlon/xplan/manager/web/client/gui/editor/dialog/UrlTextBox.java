d9f2d596a82b9581ff958352d8cafccebb18be39
Lyn Elisa Goltz <goltz@lat-lon.de>
2021-11-18 15:49:15 +0100

    XPLANBOX-491 - added missing class

---
+++ xplan-webapps/xplan-manager-web/src/main/java/de/latlon/xplan/manager/web/client/gui/editor/dialog/UrlTextBox.java
@@ -0,0 +1,20 @@
+package de.latlon.xplan.manager.web.client.gui.editor.dialog;
+
+import com.google.gwt.user.client.ui.TextBox;
+import de.latlon.xplan.manager.web.client.gui.widget.Validable;
+
+/**
+ * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
+ */
+public class UrlTextBox extends TextBox implements Validable {
+
+    @Override
+    public boolean isValid() {
+        String value = getValue();
+        if ( value != null && !value.isEmpty() ) {
+            return value.startsWith( "http" );
+        }
+        return true;
+    }
+
+}
