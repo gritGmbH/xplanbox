b687335c0f5e35be7b70d559834b7278e69d93e2
Lyn Elisa Goltz <goltz@lat-lon.de>
2022-03-23 13:27:24 +0100

    fixed formatting

--- xplan-cli/xplan-update-database-cli/src/main/java/de/latlon/xplan/update/tool/ReSynthesizerTool.java
+++ xplan-cli/xplan-update-database-cli/src/main/java/de/latlon/xplan/update/tool/ReSynthesizerTool.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -145,7 +145,8 @@ public class ReSynthesizerTool {
 		opt.setRequired(false);
 		opts.addOption(opt);
 
-		opt = new Option("c", OPT_CONFIG_DIR, true, "The directory containing the manager configuration, e.g. /path/to/directory/xplan-manager-config");
+		opt = new Option("c", OPT_CONFIG_DIR, true,
+				"The directory containing the manager configuration, e.g. /path/to/directory/xplan-manager-config");
 		opt.setRequired(true);
 		opts.addOption(opt);
 
