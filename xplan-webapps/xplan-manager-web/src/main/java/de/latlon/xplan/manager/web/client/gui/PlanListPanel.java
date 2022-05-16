029472d74b4e0125c42672af64ac7eda0d37ee4c
Lyn Elisa Goltz <goltz@lat-lon.de>
2022-05-16 16:23:53 +0200

    XPLANBOX-572 - support transformation to INSPIRE PLU

--- xplan-webapps/xplan-manager-web/src/main/java/de/latlon/xplan/manager/web/client/gui/PlanListPanel.java
+++ xplan-webapps/xplan-manager-web/src/main/java/de/latlon/xplan/manager/web/client/gui/PlanListPanel.java
@@ -724,7 +724,8 @@ public class PlanListPanel extends DecoratorPanel {
 	private boolean isVersionSupportedByInpirePlu(XPlan xPlan) {
 		return "XPLAN_41".equals(xPlan.getVersion()) || "XPLAN_50".equals(xPlan.getVersion())
 				|| "XPLAN_51".equals(xPlan.getVersion()) || "XPLAN_52".equals(xPlan.getVersion())
-				|| "XPLAN_53".equals(xPlan.getVersion()) || "XPLAN_54".equals(xPlan.getVersion());
+				|| "XPLAN_53".equals(xPlan.getVersion()) || "XPLAN_54".equals(xPlan.getVersion())
+				|| "XPLAN_60".equals(xPlan.getVersion());
 	}
 
 	private String translateVersion(String version) {
