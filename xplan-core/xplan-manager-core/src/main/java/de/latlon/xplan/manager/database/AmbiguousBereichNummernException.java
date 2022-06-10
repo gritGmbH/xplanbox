5325d9a778e4a79f0e93692e797067c51b810d11
Lyn Elisa Goltz <goltz@lat-lon.de>
2022-06-10 08:11:53 +0200

    XPLANBOX-958 - impoved exception message

---
+++ xplan-core/xplan-manager-core/src/main/java/de/latlon/xplan/manager/database/AmbiguousBereichNummernException.java
@@ -0,0 +1,34 @@
+/*-
+ * #%L
+ * xplan-manager-core - XPlan Manager Core Komponente
+ * %%
+ * Copyright (C) 2008 - 2022 lat/lon GmbH, info@lat-lon.de, www.lat-lon.de
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package de.latlon.xplan.manager.database;
+
+/**
+ * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
+ */
+public class AmbiguousBereichNummernException extends Exception {
+
+	public AmbiguousBereichNummernException(String nummer) {
+		super(String.format(
+				"Der Plan beinhaltet verschiedene Bereiche mit gleichen Nummern, die Nummer %s ist mehrfach vergeben",
+				nummer));
+	}
+
+}
