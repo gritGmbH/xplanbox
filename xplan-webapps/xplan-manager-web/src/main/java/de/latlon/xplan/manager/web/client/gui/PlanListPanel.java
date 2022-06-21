88a47ffd00dfd1e138fbeb0d7ece42a1bf5af89c
Dirk Stenger <stenger@lat-lon.de>
2022-06-21 11:54:01 +0200

    XPLANBOX-1000 - Enable button to edit RPPlan

--- xplan-webapps/xplan-manager-web/src/main/java/de/latlon/xplan/manager/web/client/gui/PlanListPanel.java
+++ xplan-webapps/xplan-manager-web/src/main/java/de/latlon/xplan/manager/web/client/gui/PlanListPanel.java
@@ -722,7 +722,8 @@ public class PlanListPanel extends DecoratorPanel {
 					|| "XPLAN_51".equals(xPlan.getVersion()) || "XPLAN_52".equals(xPlan.getVersion())
 					|| "XPLAN_53".equals(xPlan.getVersion()) || "XPLAN_54".equals(xPlan.getVersion())
 					|| "XPLAN_60".equals(xPlan.getVersion());
-		else if ("FP_Plan".equals(xPlan.getType()) || "SO_Plan".equals(xPlan.getType()))
+		else if ("FP_Plan".equals(xPlan.getType()) || "SO_Plan".equals(xPlan.getType())
+				|| "RP_Plan".equals(xPlan.getType()))
 			return "XPLAN_50".equals(xPlan.getVersion()) || "XPLAN_51".equals(xPlan.getVersion())
 					|| "XPLAN_52".equals(xPlan.getVersion()) || "XPLAN_53".equals(xPlan.getVersion())
 					|| "XPLAN_54".equals(xPlan.getVersion()) || "XPLAN_60".equals(xPlan.getVersion());
@@ -731,7 +732,7 @@ public class PlanListPanel extends DecoratorPanel {
 
 	private boolean isTypeSupportedByEditor(XPlan xPlan) {
 		return "BP_Plan".equals(xPlan.getType()) || "FP_Plan".equals(xPlan.getType())
-				|| "SO_Plan".equals(xPlan.getType());
+				|| "SO_Plan".equals(xPlan.getType()) || "RP_Plan".equals(xPlan.getType());
 	}
 
 	private boolean isVersionSupportedByInpirePlu(XPlan xPlan) {
