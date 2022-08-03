a1aa38d0e211af70d61e432da409ee2d36279672
Lyn Elisa Goltz <goltz@lat-lon.de>
2022-08-03 15:19:06 +0200

    XPLANBOX-1160 - added and configured Converter and Provider to convert comma separated string values

--- xplan-api/xplan-api-validator/src/main/java/de/latlon/xplanbox/api/validator/ApplicationPathConfig.java
+++ xplan-api/xplan-api-validator/src/main/java/de/latlon/xplanbox/api/validator/ApplicationPathConfig.java
@@ -69,6 +69,7 @@ public class ApplicationPathConfig extends ResourceConfig {
 		packages("de.latlon.xplanbox.api.validator.handler");
 		packages("de.latlon.xplanbox.api.validator.v1");
 		packages("de.latlon.xplanbox.api.commons.exception");
+		packages("de.latlon.xplanbox.api.commons.converter");
 		OpenAPI openApi = new OpenAPI();
 		openApi.setInfo(new Info().title("XPlanValidatorAPI").version("1.0.0").description("XPlanValidator REST API")
 				.termsOfService(getTermsOfService(validatorApiConfiguration))
