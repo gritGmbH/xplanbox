9728a6e7357073aedbd17927b4cab73baf05325a
Dirk Stenger <stenger@lat-lon.de>
2022-09-30 22:20:28 +0200

    XPLANBOX-1341 - Update masterportal files from Basic folder

--- xplan-webapps/xplan-validator-web/src/main/webapp/masterportal/config.js
+++ xplan-webapps/xplan-validator-web/src/main/webapp/masterportal/config.js
@@ -1,29 +1,25 @@
-/* eslint-disable no-unused-vars */
-
 const Config = {
-    wfsImgPath: "./ressources/img/",
+    wfsImgPath: "./resources/img/",
     namedProjections: [
         ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
     ],
-    /*
     footer: {
         urls: [
             {
-                "bezeichnung": "Kartographie und Gestaltung: ",
-                "url": "https://www.geoinfo.hamburg.de/",
+                "bezeichnung": "common:modules.footer.designation",
+                "url": "https://geoinfo.hamburg.de/",
                 "alias": "Landesbetrieb Geoinformation und Vermessung",
                 "alias_mobil": "LGV"
             }
-        ]
+        ],
+        showVersion: true
     },
-    */
     quickHelp: {
-        imgPath: "./ressources/img/"
+        imgPath: "./resources/img/"
     },
-    portalConf: "./config",
-    layerConf: "./config/services-internet.json",
-    restConf: "./ressources/rest-services-internet.json",
-    styleConf: "./ressources/style_v2.json",
+    layerConf: "./resources/services-internet.json",
+    restConf: "./resources/rest-services-internet.json",
+    styleConf: "./resources/style_v3.json",
     scaleLine: true,
     mouseHover: {
         numFeaturesToShow: 2,
@@ -31,4 +27,7 @@ const Config = {
     }
 };
 
-/* eslint-enable no-unused-vars */
+// conditional export to make config readable by e2e tests
+if (typeof module !== "undefined") {
+    module.exports = Config;
+}
