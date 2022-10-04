b3a146f0ab79788f321c3d2b1ea3a457fd91e11e
Dirk Stenger <stenger@lat-lon.de>
2022-10-04 22:03:28 +0200

    XPLANBOX-1341 - Update configuration of masterportal

--- xplan-webapps/xplan-validator-web/src/main/webapp/masterportal/config.js
+++ xplan-webapps/xplan-validator-web/src/main/webapp/masterportal/config.js
@@ -3,21 +3,24 @@ const Config = {
     namedProjections: [
         ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
     ],
+    /*
     footer: {
         urls: [
             {
-                "bezeichnung": "common:modules.footer.designation",
-                "url": "https://geoinfo.hamburg.de/",
+                "bezeichnung": "Kartographie und Gestaltung: ",
+                "url": "https://www.geoinfo.hamburg.de/",
                 "alias": "Landesbetrieb Geoinformation und Vermessung",
                 "alias_mobil": "LGV"
             }
         ],
         showVersion: true
     },
+    */
     quickHelp: {
         imgPath: "./resources/img/"
     },
-    layerConf: "./resources/services-internet.json",
+    portalConf: "config/",
+    layerConf: "./config/services-internet.json",
     restConf: "./resources/rest-services-internet.json",
     styleConf: "./resources/style_v3.json",
     scaleLine: true,
