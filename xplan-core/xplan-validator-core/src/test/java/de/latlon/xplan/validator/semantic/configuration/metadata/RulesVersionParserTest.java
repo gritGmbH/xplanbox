c80a8a75e076d2aab1092669d879656085d4ac84
Torsten Friebe <torsten.friebe@gmail.com>
2022-10-28 10:37:36 +0200

    XPLANBOX-1449 minor improvements, test for RulesVersionParser added

---
+++ xplan-core/xplan-validator-core/src/test/java/de/latlon/xplan/validator/semantic/configuration/metadata/RulesVersionParserTest.java
@@ -0,0 +1,46 @@
+/*-
+ * #%L
+ * xplan-validator-core - XPlan Validator Core Komponente
+ * %%
+ * Copyright (C) 2008 - 2022 lat/lon GmbH, info@lat-lon.de, www.lat-lon.de
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package de.latlon.xplan.validator.semantic.configuration.metadata;
+
+import org.junit.Test;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * @author <a href="mailto:friebe@lat-lon.de">Torsten Friebe</a>
+ */
+public class RulesVersionParserTest {
+
+	@Test
+	public void verifyThatParseVersionReturnsValue() throws URISyntaxException {
+		RulesVersionParser rulesVersionParser = new RulesVersionParser();
+		RulesVersion rulesVersion = rulesVersionParser
+				.parserRulesVersion(Path.of(RulesVersionParserTest.class.getResource("/rules").toURI()));
+		assertThat(rulesVersion.getVersion(), is("0.0.1"));
+		assertThat(rulesVersion.getSource(), containsString("xplan-validator-core"));
+	}
+
+}
