af9ee8fbb6dca9bfc70b3925e1a031600b13752d
Lyn Elisa Goltz <goltz@lat-lon.de>
2023-02-01 12:45:20 +0100

    #8768 (XPLANBOX-1562) - updated licences

--- xplan-resources/xplan-sql-scripts/src/main/resources/pre/xplansyn/create.sql
+++ xplan-resources/xplan-sql-scripts/src/main/resources/pre/xplansyn/create.sql
@@ -1,21 +1,23 @@
-/* ---   #%L
-  xplanbox-syn - Automatisiert erzeugte Konfiguration
-  %%
-  Copyright (C) 2008 - 2022 lat/lon GmbH, info@lat-lon.de, www.lat-lon.de
-  %%
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Affero General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU Affero General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  #L% --- */
+---
+-- #%L
+-- xplan-sql-scripts - SQL Skripte zum Aufsetzen der Datenhaltung.
+-- %%
+-- Copyright (C) 2008 - 2023 Freie und Hansestadt Hamburg, developed by lat/lon gesellschaft für raumbezogene Informationssysteme mbH
+-- %%
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- #L%
+---
 /* --- Diese Datei wird automatisiert generiert! Aenderungen sollten nicht manuell vorgenommen werden! Datei wurde generiert am 24.01.2023, 16:33:22 --- */
 CREATE SCHEMA xplansynpre; 
 SET search_path TO xplansynpre,public;
