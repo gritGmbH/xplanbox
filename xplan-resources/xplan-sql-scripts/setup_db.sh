f4dedf3b8d26a8808aef1cb49d8a9d85197f1df0
Torsten Friebe <torsten.friebe@gmail.com>
2023-03-01 20:22:30 +0100

    XPLANBOX-1769 -- reworked init script

--- xplan-resources/xplan-sql-scripts/setup_db.sh
+++ xplan-resources/xplan-sql-scripts/setup_db.sh
@@ -3,23 +3,26 @@
 set -e
 
 ##### CONSTANTS
-PORT=5432
 POSTGRES_USER=postgres
+DB_NAME=xplanbox
 
-if [ "$( psql -tAc "SELECT 1 FROM pg_database WHERE datname='xplanbox'" )" = '1' ]
+if [ "$( psql -U $POSTGRES_USER -tAc "SELECT 1 FROM pg_database WHERE datname='$DB_NAME'" )" = '1' ]
 then
-    echo "Database xplanbox already exists"
+    echo "Database $DB_NAME already exists"
 else
-    echo "Database xplanbox does not exist. Will be created..."
+    echo "Database $DB_NAME does not exist. Will be created..."
     
-    #Create ```INSPIRE``` databases:
-    echo "01: create db with postgis extension"
-    psql -q -p $PORT -U $POSTGRES_USER -c "CREATE DATABASE xplanbox"
+    echo "01: create db $DB_NAME"
+    psql -q -U $POSTGRES_USER -c "CREATE DATABASE $DB_NAME;"
 
-    echo "02: setup xplan schemas will be done with liquibase by xplan-db-updater"
+    echo "02: install postgis extension"
+    psql -q -U $POSTGRES_USER -d $DB_NAME -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
-    echo "03: setup inspireplu schema"
-    #psql -q -p $PORT -U $POSTGRES_USER -d xplanbox -f /xplan-sql-scripts/inspireplu/01_create_inspireplu_schema.sql
-    #psql -q -p $PORT -U $POSTGRES_USER -d xplanbox -f /xplan-sql-scripts/inspireplu/02_create_inspireplu_view.sql
+    echo "03: create user xplanbox"
+    psql -q -U $POSTGRES_USER -c "CREATE USER xplanbox WITH PASSWORD 'xplanbox' NOCREATEDB;"
 
+    echo "04: grant permissions to user xplanbox on database $DB_NAME"
+    psql -q -U $POSTGRES_USER -c "GRANT CONNECT ON DATABASE $DB_NAME TO xplanbox;"
+    psql -q -U $POSTGRES_USER -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO xplanbox;"
+    psql -q -U $POSTGRES_USER -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO xplanbox;"
 fi
