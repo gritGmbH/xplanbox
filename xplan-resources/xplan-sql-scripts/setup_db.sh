8654f045062da4f0a467f6f2659ed66ca7a1ff5e
Torsten Friebe <torsten.friebe@gmail.com>
2023-03-02 13:25:14 +0100

    XPLANBOX-1769  moved grant privileges from setup_db.sh to liquibase script

--- xplan-resources/xplan-sql-scripts/setup_db.sh
+++ xplan-resources/xplan-sql-scripts/setup_db.sh
@@ -2,27 +2,31 @@
 
 set -e
 
-##### CONSTANTS
-POSTGRES_USER=postgres
+# CONSTANTS for setting environment variables used by psql client
+PGUSER=postgres
 DB_NAME=xplanbox
 
-if [ "$( psql -U $POSTGRES_USER -tAc "SELECT 1 FROM pg_database WHERE datname='$DB_NAME'" )" = '1' ]
+if [ "$( psql -tAc "SELECT 1 FROM pg_database WHERE datname='$DB_NAME'" )" = '1' ]
 then
     echo "Database $DB_NAME already exists"
 else
     echo "Database $DB_NAME does not exist. Will be created..."
     
     echo "01: create db $DB_NAME"
-    psql -q -U $POSTGRES_USER -c "CREATE DATABASE $DB_NAME;"
+    psql -q -c "CREATE DATABASE $DB_NAME;"
 
     echo "02: install postgis extension"
-    psql -q -U $POSTGRES_USER -d $DB_NAME -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+    psql -q -d $DB_NAME -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
     echo "03: create user xplanbox"
-    psql -q -U $POSTGRES_USER -c "CREATE USER xplanbox WITH PASSWORD 'xplanbox' NOCREATEDB;"
+    psql -q -c "CREATE USER xplanbox WITH PASSWORD 'xplanbox' NOCREATEDB;"
 
     echo "04: grant permissions to user xplanbox on database $DB_NAME"
-    psql -q -U $POSTGRES_USER -c "GRANT CONNECT ON DATABASE $DB_NAME TO xplanbox;"
-    psql -q -U $POSTGRES_USER -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO xplanbox;"
-    psql -q -U $POSTGRES_USER -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO xplanbox;"
+    psql -q -c "GRANT CONNECT ON DATABASE $DB_NAME TO xplanbox;"
+    psql -q -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO xplanbox;"
+    psql -q -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO xplanbox;"
+    psql -q -c "GRANT USAGE ON SCHEMA public TO xplanbox;"
 fi
+
+
+
