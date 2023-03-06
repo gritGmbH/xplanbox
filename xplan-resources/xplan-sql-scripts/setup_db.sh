017e07b4def25e2ee28655e6928de7c4f426eabb
Lyn Elisa Goltz <goltz@lat-lon.de>
2023-03-06 15:38:45 +0100

    XPLANBOX-1763 - added user initxplanbox

--- xplan-resources/xplan-sql-scripts/setup_db.sh
+++ xplan-resources/xplan-sql-scripts/setup_db.sh
@@ -18,10 +18,16 @@ else
     echo "02: install postgis extension"
     psql -q -d $DB_NAME -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
-    echo "03: create user xplanbox"
+    echo "03: create user initxplanbox"
+    psql -q -c "CREATE USER initxplanbox WITH PASSWORD 'initxplanbox' NOCREATEDB;"
+
+    echo "04: grant permissions to user initxplanbox on database $DB_NAME"
+    psql -q -c "ALTER DATABASE $DB_NAME OWNER TO initxplanbox;"
+
+    echo "05: create user xplanbox"
     psql -q -c "CREATE USER xplanbox WITH PASSWORD 'xplanbox' NOCREATEDB;"
 
-    echo "04: grant permissions to user xplanbox on database $DB_NAME"
+    echo "06: grant permissions to user xplanbox on database $DB_NAME"
     psql -q -c "GRANT CONNECT ON DATABASE $DB_NAME TO xplanbox;"
     psql -q -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO xplanbox;"
     psql -q -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO xplanbox;"
