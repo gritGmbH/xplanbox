b00d8826fd699d997b817f6821d0457a03e74413
Torsten Friebe <torsten.friebe@gmail.com>
2023-03-06 19:53:38 +0100

    XPLANBOX-1774 rework privileges for xplanboxinit and xplanbox database user roles

--- xplan-resources/xplan-sql-scripts/setup_db.sh
+++ xplan-resources/xplan-sql-scripts/setup_db.sh
@@ -19,19 +19,20 @@ else
     psql -q -d $DB_NAME -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
     echo "03: create user initxplanbox"
-    psql -q -c "CREATE USER initxplanbox WITH PASSWORD 'initxplanbox' NOCREATEDB;"
+    psql -q -c "CREATE USER initxplanbox LOGIN NOCREATEDB INHERIT PASSWORD 'initxplanbox';"
 
     echo "04: grant permissions to user initxplanbox on database $DB_NAME"
     psql -q -c "ALTER DATABASE $DB_NAME OWNER TO initxplanbox;"
+    psql -q -c "ALTER USER initxplanbox WITH SUPERUSER;"
 
     echo "05: create user xplanbox"
-    psql -q -c "CREATE USER xplanbox WITH PASSWORD 'xplanbox' NOCREATEDB;"
+    psql -q -c "CREATE USER xplanbox LOGIN NOCREATEDB NOINHERIT PASSWORD 'xplanbox';"
 
     echo "06: grant permissions to user xplanbox on database $DB_NAME"
     psql -q -c "GRANT CONNECT ON DATABASE $DB_NAME TO xplanbox;"
-    psql -q -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO xplanbox;"
-    psql -q -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO xplanbox;"
+    psql -q -c "ALTER DEFAULT PRIVILEGES FOR USER xplanbox IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO xplanbox;"
     psql -q -c "GRANT USAGE ON SCHEMA public TO xplanbox;"
+    psql -q -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO xplanbox;"
 fi
 
 
