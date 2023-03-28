593d0075721707e7b18abc6f58e71366619dced4
Torsten Friebe <torsten.friebe@gmail.com>
2023-03-28 17:50:57 +0200

    XPLANBOX-1783 adopted setup script to devops template

--- xplan-resources/xplan-sql-scripts/setup_db.sh
+++ xplan-resources/xplan-sql-scripts/setup_db.sh
@@ -16,25 +16,17 @@ then
 else
     echo "Database $DB_NAME does not exist. Will be created..."
     
-    echo "01: create db $DB_NAME"
-    psql -q -c "CREATE DATABASE \"$DB_NAME\"";
+    echo "01: create init user $DB_INIT_USER and app user $DB_USER"
+    psql -q -c "CREATE USER \"$DB_INIT_USER\" PASSWORD '$DB_INIT_PASSWORD';"
+    psql -q -c "CREATE USER \"$DB_USER\" PASSWORD '$DB_PASSWORD';"
 
-    echo "02: install postgis extension"
+    echo "02: create db $DB_NAME with owner $DB_INIT_USER"
+    psql -q -c "GRANT \"$DB_INIT_USER\" TO \"$PGUSER\";"
+    psql -q -c "CREATE DATABASE \"$DB_NAME\" OWNER \"$DB_INIT_USER\";"
+
+    echo "03: install postgis extension"
     psql -q -d $DB_NAME -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
-    echo "03: create user $DB_INIT_USER"
-    psql -q -c "CREATE USER \"$DB_INIT_USER\" LOGIN NOCREATEDB INHERIT PASSWORD '$DB_INIT_PASSWORD';"
-
-    echo "04: grant permissions to user $DB_INIT_USER on database $DB_NAME"
-    psql -q -c "ALTER DATABASE \"$DB_NAME\" OWNER TO \"$DB_INIT_USER\";"
-    psql -q -c "ALTER USER \"$DB_INIT_USER\" WITH SUPERUSER;"
-
-    echo "05: create user $DB_USER"
-    psql -q -c "CREATE USER \"$DB_USER\" LOGIN NOCREATEDB NOINHERIT PASSWORD '$DB_PASSWORD';"
-
-    echo "06: grant permissions to user $DB_USER on database $DB_NAME"
+    echo "04: grant connect to user $DB_USER on database $DB_NAME"
     psql -q -c "GRANT CONNECT ON DATABASE \"$DB_NAME\" TO \"$DB_USER\";"
-    psql -q -c "ALTER DEFAULT PRIVILEGES FOR USER \"$DB_USER\" IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO \"$DB_USER\";"
-    psql -q -c "GRANT USAGE ON SCHEMA public TO \"$DB_USER\";"
-    psql -q -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO \"$DB_USER\";"
 fi
