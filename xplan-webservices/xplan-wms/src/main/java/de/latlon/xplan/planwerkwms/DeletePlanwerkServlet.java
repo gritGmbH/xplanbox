cd148fda521214d2cc97ed005c1901c98a8b06c0
Lyn Elisa Goltz <goltz@lat-lon.de>
2023-07-18 08:15:10 +0200

    XPLANBOX-2058 - DELETE planwerkwmsapi set 403 if SecurityException is thrown

--- xplan-webservices/xplan-wms/src/main/java/de/latlon/xplan/planwerkwms/DeletePlanwerkServlet.java
+++ xplan-webservices/xplan-wms/src/main/java/de/latlon/xplan/planwerkwms/DeletePlanwerkServlet.java
@@ -21,6 +21,7 @@
 package de.latlon.xplan.planwerkwms;
 
 import de.latlon.xplan.planwerkwms.jaxb.Planwerk;
+import org.apache.commons.io.IOUtils;
 import org.deegree.services.OWS;
 import org.deegree.services.OWSProvider;
 import org.deegree.services.config.ApiKey;
@@ -31,6 +32,7 @@ import org.deegree.workspace.Workspace;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -43,8 +45,14 @@ public class DeletePlanwerkServlet extends HttpServlet {
 	private static ApiKey apiKey = new ApiKey();
 
 	@Override
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
-		apiKey.validate(req);
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		try {
+			apiKey.validate(req);
+		}
+		catch (SecurityException e) {
+			resp.setStatus(403);
+			IOUtils.write("There were security concerns: " + e.getLocalizedMessage() + "\n", resp.getOutputStream());
+		}
 		String pathInfo = req.getPathInfo();
 		if (pathInfo != null) {
 			int planId = Integer.parseInt(pathInfo.substring(1));
