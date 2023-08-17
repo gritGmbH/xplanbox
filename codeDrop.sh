0ce4f1321aafaa12506b65abd3a628ed85bbd2fd
Lyn Elisa Goltz <goltz@lat-lon.de>
2023-08-17 07:43:52 +0200

    XPLANBOX-2277 - added param src branch

--- codeDrop.sh
+++ codeDrop.sh
@@ -1,14 +1,15 @@
 # e.g. 1840
 ticketIdNo=${1:-}
-echo "create code drop, ticketId is XPLANBOX-$ticketIdNo"
+srcBranchOrTag=${2:-origin/main}
+echo "create code drop, ticketId is XPLANBOX-$ticketIdNo, source branch is $srcBranchOrTag"
 branchName=xplanbox-$ticketIdNo-$(date +'%m-%d')-code-drop
 git fetch remote-opencode
 git checkout -b $branchName remote-opencode/main
 previousCommit=`git rev-parse HEAD`
 git fetch origin
-git merge --strategy-option theirs --allow-unrelated-histories origin/main
+git merge --strategy-option theirs --allow-unrelated-histories $srcBranchOrTag
 git reset $previousCommit
 git add -A
-lastMainCommit=`git rev-parse origin/main`
+lastMainCommit=`git rev-parse $srcBranchOrTag`
 git commit -m "code drop 6.1-SNAPSHOT: $lastMainCommit"
 git push remote-opencode $branchName
