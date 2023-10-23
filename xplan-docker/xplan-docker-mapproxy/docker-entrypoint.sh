fc9fe85520313970ea9e7b4bc3c7e525e8865a20
Torsten Friebe <torsten.friebe@gmail.com>
2023-10-23 19:35:20 +0200

    XPLANBOX-2347 fix dockerfile and entrypoint script

--- xplan-docker/xplan-docker-mapproxy/docker-entrypoint.sh
+++ xplan-docker/xplan-docker-mapproxy/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -e
+
 echo "[$(date -Iseconds)] MapProxy config initialization..."
-cp /xplan-docker-mapproxy/xplan-mapproxy-config/mapproxy.yaml /srv/mapproxy/mapproxy.yaml
+
+defined_envs="$(printf '${%s} ' $(env | cut -d'=' -f1))"
+envsubst "$defined_envs" < /xplan-docker-mapproxy/xplan-mapproxy-config/mapproxy.yaml > /srv/mapproxy/mapproxy.yaml
 
 echo "[$(date -Iseconds)] MapProxy startup..."
-exec "uwsgi --ini /srv/mapproxy/uwsgi.ini --plugin python311,cheaper_busyness"
+exec uwsgi --ini /srv/mapproxy/uwsgi.ini --plugin python311,cheaper_busyness
