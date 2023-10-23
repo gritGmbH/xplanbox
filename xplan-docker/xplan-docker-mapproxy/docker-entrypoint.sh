b1caecdf86ed26b5d66b4fd591a894ba383c6f4b
Torsten Friebe <torsten.friebe@gmail.com>
2023-10-23 17:41:39 +0200

    XPLANBOX-2347 added mapproxy config to docker image and set entry point to startup script

--- xplan-docker/xplan-docker-mapproxy/docker-entrypoint.sh
+++ xplan-docker/xplan-docker-mapproxy/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "MapProxy config initialization..."
+echo "[$(date -Iseconds)] MapProxy config initialization..."
+cp /xplan-docker-mapproxy/xplan-mapproxy-config/mapproxy.yaml /srv/mapproxy/mapproxy.yaml
 
-echo "MapProxy startup..."
+echo "[$(date -Iseconds)] MapProxy startup..."
+exec "uwsgi --ini /srv/mapproxy/uwsgi.ini --plugin python311,cheaper_busyness"
