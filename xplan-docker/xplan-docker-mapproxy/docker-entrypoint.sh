50b242768cab3f697fe113d475a9b7e4cd72c907
Torsten Friebe <torsten.friebe@gmail.com>
2023-11-02 16:29:56 +0100

    XPLANBOX-2348 updated seed.yaml config for mapproxy

--- xplan-docker/xplan-docker-mapproxy/docker-entrypoint.sh
+++ xplan-docker/xplan-docker-mapproxy/docker-entrypoint.sh
@@ -6,6 +6,7 @@ echo "[$(date -Iseconds)] MapProxy config initialization..."
 
 defined_envs="$(printf '${%s} ' $(env | cut -d'=' -f1))"
 envsubst "$defined_envs" < /xplan-docker-mapproxy/xplan-mapproxy-config/mapproxy.yaml > /srv/mapproxy/mapproxy.yaml
+envsubst "$defined_envs" < /xplan-docker-mapproxy/xplan-mapproxy-config/seed.yaml > /srv/mapproxy/seed.yaml
 
 echo "[$(date -Iseconds)] MapProxy startup..."
 exec uwsgi --ini /srv/mapproxy/uwsgi.ini
