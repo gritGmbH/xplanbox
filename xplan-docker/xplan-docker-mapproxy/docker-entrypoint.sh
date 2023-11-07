e5819b7b6acf74335a34a52522bc34aeb32d55ee
Marc Guillemot <guillemot@lat-lon.de>
2023-11-07 15:59:29 +0100

    XPLANBOX-1879 entry point handle webserver & seed commands

--- xplan-docker/xplan-docker-mapproxy/docker-entrypoint.sh
+++ xplan-docker/xplan-docker-mapproxy/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+executionMode=$1 
 
 echo "[$(date -Iseconds)] MapProxy config initialization..."
 
@@ -8,5 +9,11 @@ defined_envs="$(printf '${%s} ' $(env | cut -d'=' -f1))"
 envsubst "$defined_envs" < /xplan-docker-mapproxy/xplan-mapproxy-config/mapproxy.yaml > /srv/mapproxy/mapproxy.yaml
 envsubst "$defined_envs" < /xplan-docker-mapproxy/xplan-mapproxy-config/seed.yaml > /srv/mapproxy/seed.yaml
 
-echo "[$(date -Iseconds)] MapProxy startup..."
-exec uwsgi --ini /srv/mapproxy/uwsgi.ini
+if [ "$executionMode" == "seed" ]
+then
+	echo "[$(date -Iseconds)] MapProxy seed..."
+	exec mapproxy-seed -f /srv/mapproxy/mapproxy.yaml -s /srv/mapproxy/seed.yaml -c $XPLAN_MAPPROXY_RESEED_CONCURRENCY --seed bpreseed,fpreseed
+else
+	echo "[$(date -Iseconds)] MapProxy startup..."
+	exec uwsgi --ini /srv/mapproxy/uwsgi.ini
+fi
