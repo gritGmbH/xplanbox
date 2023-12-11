680efb73135c6b42fd46fc7095d93433572a8763
latlon team <info@lat-lon.de>
2023-12-11 11:47:03 +0000

    Code drop
    - XPLANBOX-2574 - renamed grid gdi_de_3857, changed resolutions (5dc6bc64a)
    - XPLANBOX-2574 - updated exec in docker-entrypoint.sh (6863a6a01)
    - XPLANBOX-2574 - configured/added cache per plan type and CRS (67176492b)
    - XPLANBOX-2575 - added PIXELSIZE=0.28001 (63973be25)
    - XPLANBOX-2574 - added grid gdi_de_3857 (8e08247c8)
    
    Co-authored-by: Lyn Elisa Goltz <goltz@lat-lon.de>
    
    Dropped from commit: 2632ece2c310f32d6ccda49591571947067a8573

--- xplan-docker/xplan-docker-mapproxy/docker-entrypoint.sh
+++ xplan-docker/xplan-docker-mapproxy/docker-entrypoint.sh
@@ -12,7 +12,7 @@ envsubst "$defined_envs" < /xplan-docker-mapproxy/xplan-mapproxy-config/seed.yam
 if [ "$executionMode" == "seed" ]
 then
 	echo "[$(date -Iseconds)] MapProxy seed..."
-	exec mapproxy-seed -f /srv/mapproxy/mapproxy.yaml -s /srv/mapproxy/seed.yaml -c $XPLAN_MAPPROXY_RESEED_CONCURRENCY --seed bpreseed,fpreseed,lpreseed,rpreseed,soreseed
+	exec mapproxy-seed -f /srv/mapproxy/mapproxy.yaml -s /srv/mapproxy/seed.yaml -c $XPLAN_MAPPROXY_RESEED_CONCURRENCY --seed bpreseed_25832,fpreseed_25832,lpreseed_25832,rpreseed_25832,soreseed_25832,bpreseed_3857,fpreseed_3857,lpreseed_3857,rpreseed_3857,soreseed_3857
 else
 	echo "[$(date -Iseconds)] MapProxy startup..."
 	exec uwsgi --ini /srv/mapproxy/uwsgi.ini
